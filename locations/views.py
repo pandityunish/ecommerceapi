ec7336ed1dbc60cf964fd41f332ec1914ecad082
kripas1369 <kripask50@gmail.com>
2023-05-30 19:06:44 +0545

    loactions bug fixed

--- locations/views.py
+++ locations/views.py
@@ -2,9 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Location
 from .serializers import LocationSerializer
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 
 class LocationAPIView(APIView):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+    def get_queryset(self):
+        return Location.objects.all()
+
     def get(self, request):
-        locations = Location.objects.all()
-        serializer = LocationSerializer(locations, many=True)
+        queryset = self.get_queryset()
+        serializer = LocationSerializer(queryset, many=True)
         return Response(serializer.data)
